69354e077866968eee6df98771a79e503a872d6a
kaya18 <kaya18@users.noreply.github.com>
2017-08-22 17:02:12 +0200

    Update SplashScreen.java

--- src/android/SplashScreen.java
+++ src/android/SplashScreen.java
@@ -374,6 +374,9 @@ public class SplashScreen extends CordovaPlugin {
                 progressBar.setPadding(0, (height / 4), 0, 0);
                 centeredLayout.addView(progressBar);
                 centeredLayout.setPadding(0,(height / 4), 0, 0);
+                   
+                DialogFragment dialog = new MyDialogFragment();
+                dialog.show(getSupportFragmentManager(), "height: "+height;
 
                 spinnerDialog.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
                 spinnerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
