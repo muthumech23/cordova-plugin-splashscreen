a251fb9df93572dc290de4a977bc33937d9c83ef
kaya18 <kaya18@users.noreply.github.com>
2017-08-22 15:30:16 +0200

    Update SplashScreen.java

--- src/android/SplashScreen.java
+++ src/android/SplashScreen.java
@@ -362,7 +362,7 @@ public class SplashScreen extends CordovaPlugin {
 
                 ProgressBar progressBar = new ProgressBar(webView.getContext());
                 RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-                layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+                layoutParams.addRule(RelativeLayout.BELOW, RelativeLayout.TRUE);
                 progressBar.setLayoutParams(layoutParams);
 
                 centeredLayout.addView(progressBar);
